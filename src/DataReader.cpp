669495b172248102c0fa4705bad47909c5b0be23
diogo <diogo.campeao@gmail.com>
2025-04-12 23:37:57 +0100

    DataReader class added Changes made on truck class

--- src/DataReader.cpp
+++ src/DataReader.cpp
@@ -1,1 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include "../headers/DataReader.h"
+
+#include "../data_structures/truck.h"
+
+using namespace std;
+
+DataReader::DataReader() = default;
+DataReader::~DataReader() = default;
+
+void DataReader::readTrucks(std::string fileName, Truck& truck) {
+    ifstream file(fileName);
+    if (!file) {
+        cerr << "Error opening file " << fileName << endl;
+        exit(1);
+    }
+
+    string capacityStr, numPalletsStr;
+    double capacity;
+    int numPallets;
+
+    string line;
+    getline(file, line);
+
+    getline(file, line);
+    istringstream iss(line);
+    getline(iss, capacityStr, ',');
+    getline(iss, numPalletsStr, '\r');
+
+    capacity = stod(capacityStr);
+    numPallets = stoi(numPalletsStr);
+
+    truck.setCapacity(capacity);
+    truck.setNumPallets(numPallets);
+}
+
+
+void DataReader::readPallets(std::string& fileName, Truck& truck) {
+    ifstream file(fileName);
+    if (!file) {
+        cerr << "Error opening file " << fileName << endl;
+        exit(1);
+    }
+
+    string idStr, weightStr, valueStr;
+    int id;
+    double weight, value;
+    vector<Pallet *> pallets;
+
+    string line;
+    getline(file, line);
+
+    while (getline(file, line)) {
+        istringstream iss(line);
+
+        getline(iss, idStr, ',');
+        getline(iss, weightStr, ',');
+        getline(iss, valueStr, '\r');
+
+        id = stoi(idStr);
+        weight = stod(weightStr);
+        value = stod(valueStr);
+
+        Pallet pallet(id, weight, value);
+        pallets.push_back(&pallet);
+    }
+    truck.setPallets(pallets);
+    file.close();
+}
+
+
+
