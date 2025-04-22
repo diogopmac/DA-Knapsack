7f2aa0e91f3d881de384fa4db3d96a37a23dfefa
diogo <diogo.campeao@gmail.com>
2025-04-22 09:50:11 +0100

    Fix include (algortithms xD)

--- src/Algorithms.cpp
+++ src/Algorithms.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 
 vector<Pallet *> brute_force(Truck& truck) {
-    sol = vector<Pallet *>;
+    vector<Pallet *> sol;
     return sol;
 }
 
 vector<Pallet *> dynamic_program(Truck& truck) {
-    sol = vector<Pallet *>;
+    vector<Pallet *> sol;
     return sol;
 }
 
 vector<Pallet *> approximation(Truck& truck) {
-    sol = vector<Pallet *>;
+    vector<Pallet *> sol;
     return sol;
 }
 
 vector<Pallet *> int_linear_program(Truck& truck) {
-    sol = vector<Pallet *>;
+    vector<Pallet *> sol;
     return sol;
 }
