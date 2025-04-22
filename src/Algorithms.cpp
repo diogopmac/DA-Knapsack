e38359df7224e13766bf8d75c725603f91e39479
Hugo <up202307383@up.pt>
2025-04-22 09:33:05 +0100

    Initial version of Algorithms class

---
+++ src/Algorithms.cpp
@@ -0,0 +1,23 @@
+#include "../headers/Algorithms.h"
+
+using namespace std;
+
+vector<Pallet *> brute_force(Truck& truck) {
+    sol = vector<Pallet *>;
+    return sol;
+}
+
+vector<Pallet *> dynamic_program(Truck& truck) {
+    sol = vector<Pallet *>;
+    return sol;
+}
+
+vector<Pallet *> approximation(Truck& truck) {
+    sol = vector<Pallet *>;
+    return sol;
+}
+
+vector<Pallet *> int_linear_program(Truck& truck) {
+    sol = vector<Pallet *>;
+    return sol;
+}
